ad42aabbaeeca23414e9b162149e34248c926298
Ricardo Barbosa da Silva <ricardosevenotion@gmail.com>
2025-06-03 00:10:31 -0300

    att

--- database.sql
+++ database.sql
@@ -1,27 +1,3 @@
-
--- Tabelas
-
--- CADASTRO DE PRODUTOS
-
-CREATE DATABASE eleganceetluxe;
-USE eleganceetluxe;
-
-
-CREATE TABLE produtos (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    tipo_produto VARCHAR(100),          -- Tipo de produto (ex: vestido, decoração)
-    codigo_produto VARCHAR(50) UNIQUE,  -- Código interno do produto
-    unidade VARCHAR(10),                -- Unidade de medida (ex: UN)
-    preco_custo DECIMAL(10, 2),         -- Preço de custo do produto
-    lucro_percentual DECIMAL(5, 2),     -- Lucro percentual desejado
-    valor_min DECIMAL(10, 2),           -- Valor mínimo de venda
-    valor_max DECIMAL(10, 2),           -- Valor máximo de venda
-    descricao TEXT                      -- Descrição detalhada do produto
-);
-
-
--- CADASTRO DE PESSOAS
-
 CREATE DATABASE eleganceetluxe;
 
 
@@ -33,8 +9,11 @@ CREATE TABLE cadastro_pessoas (
     data_nascimento DATE NOT NULL,
     ddd CHAR(2) NOT NULL,
     numero_celular CHAR(9) NOT NULL,
+	email VARCHAR(255) UNIQUE NOT NULL, -- Email deve ser único
+    senha VARCHAR(255) NOT NULL, -- Para armazenar o hash da senha
     criado_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-
 select * from cadastro_pessoas;
+
+drop database eleganceetluxe;
