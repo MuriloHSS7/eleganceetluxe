9387e0dac888f3fb367b28043eb115bbadba240a
Ricardo Barbosa da Silva <ricardosevenotion@gmail.com>
2025-06-17 14:56:02 -0300

    Adicionando o js
    - Tentei

--- dashboard/index.php
+++ dashboard/index.php
@@ -73,110 +73,77 @@ $usuario = $_SESSION['email_usuario'];
 
         <!-- Produtos -->
         <div class="products">
-            <div class="product-card">
+
+            <!-- Adicionei nas divs mais atributos -->
+
+            <div class="product-card" data-preco="409.90" data-comprimento="midi" data-cor="branco" data-tamanho="m">
                 <img src="https://images.tcdn.com.br/img/img_prod/886992/vestido_longo_de_noiva_tomara_que_caia_com_sutia_interno_com_fenda_antunes_branco_9148_2_40d8daf724df58857a0a970c232e89e0.jpg" alt="Vestido branco">
                 <h4>Vestido branco</h4>
                 <p>R$ 409,90 no pix</p>
                 <p>R$ 409,90 ou 2x de R$204,95 sem juros</p>
             </div>
 
-            <div class="product-card">
-                <img src="https://images.tcdn.com.br/img/img_prod/886992/vestido_longo_de_festa_madrinhas_tomara_que_caia_busto_assimetrico_detalhe_lateral_amarracao_nas_cos_10034_2_67334c2d2fb614b89c823c25e16bfc8f.jpg"
-                    alt="Vestido preto">
+            <div class="product-card" data-preco="219.99" data-comprimento="midi" data-cor="preto" data-tamanho="g">
+                <img src="https://images.tcdn.com.br/img/img_prod/886992/vestido_longo_de_festa_madrinhas_tomara_que_caia_busto_assimetrico_detalhe_lateral_amarracao_nas_cos_10034_2_67334c2d2fb614b89c823c25e16bfc8f.jpg" alt="Vestido preto">
                 <h4>Vestido preto</h4>
                 <p>R$ 219,99 no pix</p>
                 <p>R$ 219,99 ou 2x de R$110,00 sem juros</p>
             </div>
 
-            <div class="product-card">
-                <img src="https://images.tcdn.com.br/img/img_prod/886992/vestido_longo_de_festa_madrinhas_um_ombro_com_fenda_premium_dodeny_marsala_8530_2_0d42e3cbbf22836f0f5d4b65578301d9.jpg" alt="Vestido vermelho">
+            <div class="product-card" data-preco="199.99" data-comprimento="midi" data-cor="vermelho" data-tamanho="p">
+                <img src="https://images.tcdn.com.br/img/img_prod/886992/vestido_longo_de_festa_madrinhas_um_ombro_com_fenda_premium_dodeny_marsala_8530_2_0d42e3cbbf22836f0f5d4b65578301d9.jpg" alt="Vestido vermelho Vinho">
                 <h4>Vestido vermelho Vinho</h4>
                 <p>R$ 199,99 no pix</p>
                 <p>R$ 199,99 ou 2x de R$100,00 sem juros</p>
             </div>
 
-            <div class="product-card">
+            <div class="product-card" data-preco="219.99" data-comprimento="midi" data-cor="preto" data-tamanho="g">
                 <img src="https://rouffman.com.br/cdn/shop/files/S710e37e1782b4f058665ea5ae1816aacX.webp?v=1714068120" alt="Terno preto">
                 <h4>Paletó + Colete</h4>
                 <p>R$ 219,99 no pix</p>
                 <p>R$ 219,99 ou 2x de R$110,00 sem juros</p>
             </div>
 
-            <div class="product-card">
+            <div class="product-card" data-preco="119.99" data-comprimento="curto" data-cor="vermelho" data-tamanho="m">
                 <img src="https://images.sunset.systems/.ciadovestido.com.br/AFVAP0C27C/420x630-100-0/AFVAP0C27C.jpg?ts=1749265200&mode=2" alt="Vestido curto vermelho">
                 <h4>Vestido curto vermelho</h4>
                 <p>R$ 119,99 no pix</p>
                 <p>R$ 119,99 ou 2x de R$60,00 sem juros</p>
             </div>
 
-            <div class="product-card">
+            <div class="product-card" data-preco="139.99" data-comprimento="curto" data-cor="branco" data-tamanho="p">
                 <img src="https://images.sunset.systems/.ciadovestido.com.br/7HNBA8L248/420x630-100-0/7HNBA8L248.jpg?ts=1749265200&mode=2">
                 <h4>Vestido curto branco</h4>
                 <p>R$ 139,99 no pix</p>
                 <p>R$ 139,99 ou 2x de R$70,00 sem juros</p>
             </div>
-            
-            <div class="product-card">
+
+            <div class="product-card" data-preco="129.99" data-comprimento="curto" data-cor="laranja" data-tamanho="g">
                 <img src="https://images.sunset.systems/.ciadovestido.com.br/H5TE9QT224/420x630-100-0/H5TE9QT224.jpg?ts=1749265200&mode=2">
                 <h4>Vestido curto laranja</h4>
                 <p>R$ 129,99 no pix</p>
                 <p>R$ 129,99 ou 2x de R$65,00 sem juros</p>
             </div>
 
-            <div class="product-card">
+            <div class="product-card" data-preco="169.99" data-comprimento="curto" data-cor="marrom" data-tamanho="m">
                 <img src="https://images.sunset.systems/.ciadovestido.com.br/K0L26TH224/420x630-100-0/K0L26TH224.jpg?ts=1749265200&mode=2">
                 <h4>Vestido curto marrom</h4>
                 <p>R$ 169,99 no pix</p>
                 <p>R$ 169,99 ou 2x de R$85,00 sem juros</p>
             </div>
 
-            <div class="product-card">
+            <div class="product-card" data-preco="319.99" data-comprimento="longo" data-cor="preto" data-tamanho="g">
                 <img src="https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcQWmPnQv1ICjgDThvMlZK8YILeQTLds6BMzaQrL3JylICFWvsC8mHeSotTV88lhIGF6twO_Dg64Z80b0G8hSSZRxuenr88HlWdLt-1NB4c&usqp=CAc">
                 <h4>Terno wall Black</h4>
                 <p>R$ 319,99 no pix</p>
                 <p>R$ 319,99 ou 2x de R$160,00 sem juros</p>
             </div>
 
-            <div class="product-card">
+            <div class="product-card" data-preco="69.99" data-comprimento="curto" data-cor="preto" data-tamanho="m">
                 <img src="https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcQZtKfn3SAfWaiBvxpcEN7MXgsPbLJQylMQ9N30ldbyVyRz8si9pOMsTH4NTsu3cgUmUmcv8pul6XFGoij4PZKbablfMtzmrE0VWP0uDx6CIKBRoaBQZqJI">
                 <h4>Sapato Social</h4>
                 <p>R$ 69,99 no pix</p>
                 <p>R$ 69,99 ou 2x de R$35,00 sem juros</p>
-            </div>
-
-            <div class="product-card">
-                <img src="https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcRzLZGDEj0IWzDGnQ8zc4cXCztqTca6s5U4KnspYpgrXehESV3_zK7nUMnSQPYDEROO4vEa0_ejVY_bc3CiBZwVs_WMukWoZ6W9scicCSM">
-                <h4>Calça Social</h4>
-                <p>R$ 169,99 no pix</p>
-                <p>R$ 169,99 ou 2x de R$85,00 sem juros</p>
-            </div>
-
-            <div class="product-card">
-                <img src="https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcSrpjc7I8uaUOFul3ZXgTw4HGn9FvY1wQ2npP8JCehwVkndKFBKjsX-p6Pu25h9jF3M7wh1htzPcnB5IFwG4SG27ng0dlUiB5pWhzgjDPR4FRLyAr428vFki3_iIlqwqY9im7it7NE&usqp=CAc">
-                <h4>Cinto</h4>
-                <p>R$ 29,99 no pix</p>
-                <p>R$ 29,99 ou 2x de R$15,00 sem juros</p>
-            </div>
-
-            <div class="product-card">
-                <img src="https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcTuBE0avGkR-sAiQhndkXM8my7QkL4Hd30cbvQYwwPIYC4o8qR-FsvY1-38-g11qb_184GL6KBg59h8dGlAq20u5cdHKP9jD2XQHRC-ol_GE3EGmEEN7E56">
-                <h4>Gravata</h4>
-                <p>R$ 29,99 no pix</p>
-                <p>R$ 29,99 ou 2x de R$15,00 sem juros</p>
-            </div>
-            
-            <div class="product-card">
-                <img src="https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcTVCtZgaxY05jJtuzJ3_mOrOrNw9gdWXXd0bBT1xNR_-iLozdo4RPSsZhGVV7WVizlKkv58EXrZ-PtxoWoYYf4Bg10WT50EeLAdyPEiY4V8-Tp55dhDfZwG89PMxLiwb7PNdvX87w&usqp=CAc">
-                <h4>Colete</h4>
-                <p>R$ 39,99 no pix</p>
-                <p>R$ 39,99 ou 2x de R$20,00 sem juros</p>
-            </div>
-            
-            <div class="product-card">
-                <img src="https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcSEvgCPZboz6_1RFGbXKJwT591cIlBwqAzwbH5jTIE7aTksKt9i5sLrbgJo_QXE7i1yIdAJYgNxr3MuiXKc3I16ANh4d9bRJ1xiv_1cHGwCxcpZyt6L6BwmakaLO5Ks&usqp=CAc">
-                <h4>Calça Social</h4>
-                <p>R$ 79,99 no pix</p>
-                <p>R$ 79,99 ou 2x de R$40,00 sem juros</p>
             </div>
         </div>
     </div>
@@ -189,6 +156,8 @@ $usuario = $_SESSION['email_usuario'];
             dos detentores dos direitos.</p>
         <a href="#" class="help-btn">Tire suas dúvidas</a>
     </div>
+<script src="filtros.js"></script>
+   
 </body>
 
 </html>
